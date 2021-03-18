b08b1c10ea07670ddeffc6ae1fdcc24273dd8a2e
TAKIKV <hamou22221@gmail.com>
2021-03-18 17:35:02 +0100

    first 17h1803

--- public/css/config/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  'environment': 'development',
-  'secret':      'theawesomeanduniquesecret',
-  'database':    'postgres://postgres:postgres@localhost:5432/postgres',
-  'port':         3000
-};
